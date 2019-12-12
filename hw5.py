df72d9321ffae9375497257206006becd2c1376c
luk1684tw <luk1684tw@gmail.com>
2019-12-13 04:32:59 +0800

    part1 unknown bug

--- hw5.py
+++ hw5.py
@@ -1,7 +1,9 @@
 import numpy as np
 
+
 from scipy.optimize import minimize
 from matplotlib import pyplot as plt
+
 
 def extract_gp_data():
     data = open('./input.data')
@@ -14,43 +16,72 @@ def extract_gp_data():
     return (data_points)
 
 
-def rational_quadratic(x, y, alpha, length_scale):
-    return (1 + (x - y)**2 / (2 * alpha * length_scale**2)) ** (-1 * alpha)
-
-
-def plot_gp(data_points, test_x, prd_results, fig_name):
-    plt.plot(data_points[:, 0], data_points[:, 1], 'ro')
-    plt.fill_between(x=test_x, y1=prd_results[:, 0] + 1.96*np.sqrt(prd_results[:, 1]), y2=prd_results[:, 0] - 1.96*np.sqrt(prd_results[:, 1]))
-    plt.plot(test_x, prd_results[:, 0], 'r--')
-    plt.savefig(f'{fig_name}.png')
-    return
-
-if __name__ == "__main__":
-    data_points = extract_gp_data()
-
-    # 1 Gaussian Process
-    beta = 5
+def make_C(data_points, alpha, length_scale, sigma, beta):
     C = list()
-    alpha = 1
-    length_scale = 1
-
     for i in range(34):
-        C_i = rational_quadratic(data_points[i, 0], data_points[:, 0], alpha, length_scale)
+        C_i = rational_quadratic(data_points[i, 0], data_points[:, 0], alpha, length_scale, sigma)
         C.append(C_i)
     C = np.array(C)
     C += np.identity(len(C)) / beta
-    
-    test_x = np.linspace(-60, 60, 240)
+
+    return C
+
+
+def rational_quadratic(x, y, alpha, length_scale, sigma):
+
+    return sigma**2 * (1 + (y - x)**2 / (2 * alpha * length_scale**2)) ** (-1 * alpha)
+
+
+def optimize(theta, data_points, beta):
+    C = make_C(data_points, theta[0], theta[1], theta[2], beta)
+    return 0.5 * np.log(np.linalg.det(C)) + 0.5 * np.dot(data_points[:, 1].T, np.dot(np.linalg.inv(C), data_points[:, 1])) + 0.5*len(data_points)*np.log(2*np.pi)
+
+
+def plot_gp(data_points, test_x, prd_results, fig_name):
+    plt.plot(data_points[:, 0], data_points[:, 1], 'bo')
+    plt.fill_between(x=test_x, y1=prd_results[:, 0] + 1.96*np.sqrt(prd_results[:, 1]), y2=prd_results[:, 0] - 1.96*np.sqrt(prd_results[:, 1]), color='pink')
+    plt.plot(test_x, prd_results[:, 0], linestyle='-', color='red')
+    plt.savefig(f'{fig_name}.png')
+    return
+
+
+def gp_predict(test_x, data_points, alpha, length_scale, sigma, beta):
     prd_results = list()
     for x in test_x:
-        kernel = rational_quadratic(data_points[:, 0], x, alpha, length_scale).reshape((34, 1))
-        variance = rational_quadratic(x, x, alpha, length_scale) + 1/beta
+        kernel = rational_quadratic(data_points[:, 0], x, alpha, length_scale, sigma).reshape((34, 1))
+        variance = rational_quadratic(x, x, alpha, length_scale, sigma) + 1/beta
         mu = np.dot(kernel.T, np.dot(np.linalg.inv(C), data_points[:, 1]))
         var = variance - np.dot(kernel.T, np.dot(np.linalg.inv(C), kernel))
         var = var.reshape((1))
         res = np.concatenate((mu, var), axis=0)
         prd_results.append(res)
     prd_results = np.array(prd_results)
-    plot_gp(data_points, test_x, prd_results, 'GP_origin')
+    return prd_results
 
+
+if __name__ == "__main__":
+    data_points = extract_gp_data()
+
+    # 1 Gaussian Process
+    beta = 5
+    alpha = 1
+    length_scale = 1
+    sigma = 1
+    C = make_C(data_points, alpha, length_scale, sigma, beta)
+    # print (C)
     
+    test_x = np.linspace(-60, 60, 500)
+    prd_results = gp_predict(test_x, data_points, alpha, length_scale, sigma, beta)
+    plot_gp(data_points, test_x, prd_results, 'GP_origin')
+    plt.close()
+
+    theta = np.array([alpha, length_scale, sigma])
+
+    result = minimize(optimize, theta, args=(data_points, beta))
+    print (result)
+    alpha, length_scale, sigma = result.x[0], result.x[1], result.x[2]
+
+    prd_results = gp_predict(test_x, data_points, alpha, length_scale, sigma, beta)
+    print (prd_results)
+    plot_gp(data_points, test_x, prd_results, 'GP_finetuned')
+    plt.close()
